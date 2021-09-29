d7bad1120d065e2d7a4f4428aec401e9ca7cace1
Jason Tee <teej@bc.edu>
2021-09-29 00:28:29 -0400

    almost done

---
+++ index.js
@@ -0,0 +1,18 @@
+document.getElementById("link1").addEventListener('click', () => {
+    document.getElementById("section1").scrollIntoView({behavior: 'smooth'});
+})
+document.getElementById("link2").addEventListener('click', () => {
+    document.getElementById("section2").scrollIntoView({behavior: 'smooth'});
+})
+document.getElementById("link3").addEventListener('click', () => {
+    document.getElementById("section3").scrollIntoView({behavior: 'smooth'});
+})
+document.getElementById("link4").addEventListener('click', () => {
+    document.getElementById("section4").scrollIntoView({behavior: 'smooth'});
+})
+document.getElementById("link5").addEventListener('click', () => {
+    document.getElementById("section5").scrollIntoView({behavior: 'smooth'});
+})
+document.getElementById("link6").addEventListener('click', () => {
+    document.getElementById("section6").scrollIntoView({behavior: 'smooth'});
+})
